a3046bda1ffb06fc7ca2e1abfbc9afee730a1f64
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-24 15:38:38 +0100

    oci-terraform

--- variables.tf
+++ variables.tf
@@ -3,5 +3,6 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "region" {}
 variable "compartment_ocid" {}
+variable "private_key_path" {}
 variable "ssh_public_key" {}
 variable "availability_domain" {}
