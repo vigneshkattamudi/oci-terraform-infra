4caa67d22f14bdbee1c65c4443f110e2af03f1d5
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-24 14:34:52 +0100

    oci-terraform

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "oci" {
- tenancy_ocid = var.tenancy_ocid
- user_ocid = var.user_ocid
- fingerprint = var.fingerprint
- private_key_path = var.private_key_path
- region = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = file(var.private_key_path)
+  region       = var.region
 }
 
