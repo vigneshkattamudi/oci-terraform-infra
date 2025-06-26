a1b125f3d997dc92725b9babb12abb968c5e42d5
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-26 19:54:08 +0100

    vigneshkattamudi

--- compute_instance.tf
+++ compute_instance.tf
@@ -1,25 +1,25 @@
-resource "oci_core_instance" "test" {
-  availability_domain = var.availability_domain
-  compartment_id      = var.compartment_ocid
-  display_name        = "oracle-linux-9-instance"
-  shape               = "VM.Standard.E5.Flex"
-  shape_config {
-    ocpus = 1
-    memory_in_gbs = 6
-  }
+# resource "oci_core_instance" "test" {
+#   availability_domain = var.availability_domain
+#   compartment_id      = var.compartment_ocid
+#   display_name        = "oracle-linux-9-instance"
+#   shape               = "VM.Standard.E5.Flex"
+#   shape_config {
+#     ocpus = 1
+#     memory_in_gbs = 6
+#   }
 
-  source_details {
-    source_type = "image"
-    source_id = "ocid1.image.oc1.uk-london-1.aaaaaaaa3rygccfubatxnmx2ccg66wty5drzo4vux3bvkkjoy2guc5xssaqa"
-  }
+#   source_details {
+#     source_type = "image"
+#     source_id = "ocid1.image.oc1.uk-london-1.aaaaaaaa3rygccfubatxnmx2ccg66wty5drzo4vux3bvkkjoy2guc5xssaqa"
+#   }
 
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.public_subnet.id
-    assign_public_ip = true
-  }
+#   create_vnic_details {
+#     subnet_id        = oci_core_subnet.public_subnet.id
+#     assign_public_ip = true
+#   }
 
-  metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-  }
-}
+#   metadata = {
+#     ssh_authorized_keys = var.ssh_public_key
+#   }
+# }
 
--- object_storage.tf
+++ object_storage.tf
@@ -1,8 +1,8 @@
-data "oci_objectstorage_namespace" "ns" {}
+# data "oci_objectstorage_namespace" "ns" {}
 
-resource "oci_objectstorage_bucket" "my_bucket" {
-  compartment_id = var.compartment_ocid
-  name           = "my-terraform-bucket-demo"
-  namespace      = data.oci_objectstorage_namespace.ns.namespace
-  storage_tier   = "Standard"
-}
+# resource "oci_objectstorage_bucket" "my_bucket" {
+#   compartment_id = var.compartment_ocid
+#   name           = "my-terraform-bucket-demo"
+#   namespace      = data.oci_objectstorage_namespace.ns.namespace
+#   storage_tier   = "Standard"
+# }
