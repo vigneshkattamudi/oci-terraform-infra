631f9a7a9bf0b182017d2f3d2f5904551711e881
vigneshkattamudi <vkattamudi88@gmail.com>
2025-06-27 13:22:26 +0100

    terraform

--- modules/private_instance/outputs.tf
+++ modules/private_instance/outputs.tf
@@ -1,3 +1,15 @@
+output "instance_public_ip" {
+  value = oci_core_instance.this.public_ip
+}
+
+output "instance_private_ip" {
+  value = oci_core_instance.this.private_ip
+}
+
 output "instance_id" {
   value = oci_core_instance.this.id
+}
+
+output "availability_domain" {
+  value = oci_core_instance.this.availability_domain
 }
--- ouput.tf
+++ ouput.tf
@@ -1,24 +1,19 @@
-# output "instance_public_ip" {
-#   description = "Public IP address of the compute instance"
-#   value       = oci_core_instance.public_ip
-# }
+output "private_instance_public_ip" {
+  value = module.private_instance.instance_public_ip
+}
 
-# output "instance_private_ip" {
-#   description = "Private IP address of the compute instance"
-#   value       = oci_core_instance.private_ip
-# }
+output "private_instance_private_ip" {
+  value = module.private_instance.instance_private_ip
+}
 
-# output "instance_ocid" {
-#   description = "OCID of the compute instance"
-#   value       = oci_core_instance.id
-# }
+output "private_instance_id" {
+  value = module.private_instance.instance_id
+}
 
-# output "subnet_ocid" {
-#   description = "OCID of the subnet where the instance is launched"
-#   value       = oci_core_subnet.public_subnet.id
-# }
+output "private_instance_availability_domain" {
+  value = module.private_instance.availability_domain
+}
 
-# output "availability_domain" {
-#   description = "Availability domain where the instance is launched"
-#   value       = oci_core_instance.availability_domain
-# }
+output "public_subnet_id" {
+  value = module.public_subnet.subnet_id
+}
